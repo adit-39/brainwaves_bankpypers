2444fd5dcc261f016e005a83fd68b1be124d77fb
Aditya Dalwani <adit.39@gmail.com>
2014-11-08 21:49:38 +0530

    Local Wifi Server within bank

---
+++ wifi_server/WifiServer.py
@@ -0,0 +1,17 @@
+from flask import Flask,json
+import utils
+
+app = Flask(__name__)
+
+@app.route('/api/authorize/<name>')
+def salt(name):
+	x=utils.get_salt(name)
+	return json.dumps(x)
+
+@app.route('/api/authorize/<name>/<passwd>')
+def authorize(name,passwd):
+	b=utils.verify(name,passwd)
+	return json.dumps(b)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True, port=5001)
