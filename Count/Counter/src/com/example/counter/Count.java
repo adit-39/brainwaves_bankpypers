05b28813fcce8bf9fecf706079792d44254e7099
arvind-s <arvind.s01994@gmail.com>
2014-11-09 05:43:12 +0530

    counter with notification

--- Count/Counter/src/com/example/counter/Count.java
+++ Count/Counter/src/com/example/counter/Count.java
@@ -1,9 +1,11 @@
 package com.example.counter;
 
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.NotificationCompat;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +28,27 @@ public class Count extends Activity {
 		     public void onFinish() {
 		         mTextField.setText("done!");
 		         Toast.makeText(getApplicationContext(), "crazzzzzzy`", Toast.LENGTH_LONG).show();
+		         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext());
+		         mBuilder.setSmallIcon(R.drawable.ic_launcher);
+		         mBuilder.setContentTitle("Notification Alert, Click Me!");
+		         mBuilder.setContentText("Hi, This is Android Notification Detail!");
+		         
+//		         Intent resultIntent = new Intent(this, ResultActivity.class);
+//		         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+//		         stackBuilder.addParentStack(ResultActivity.class);
+//
+//		         // Adds the Intent that starts the Activity to the top of the stack
+//		         stackBuilder.addNextIntent(resultIntent);
+//		         PendingIntent resultPendingIntent =
+//		                 stackBuilder.getPendingIntent(
+//		                     0,
+//		                     PendingIntent.FLAG_UPDATE_CURRENT
+//		                 );
+//		         mBuilder.setContentIntent(resultPendingIntent);
+		         
+		         NotificationManager mNotificationManager =
+		        		    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		         mNotificationManager.notify(100, mBuilder.build());
 		     }
 		  }.start();
 	}
